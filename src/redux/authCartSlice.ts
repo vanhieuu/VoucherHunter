7e814dfabb44740ee80dcbbcb43e732dd31f4ca9
Thân Văn Hiếu <vanhieuu99@gmail.com>
2021-12-15 13:50:45 +0700

    commit code

--- src/redux/authCartSlice.ts
+++ src/redux/authCartSlice.ts
@@ -1,55 +1,160 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import {IProduct} from '../types/IProduct';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
-export interface IItemCart {
-  cart: {
-    _id: string;
-    item: [
-      {
-        product_id: IProduct;
-      },
-    ];
-  };
+export interface IProduct {
+  _id: string;
+  name: string;
+  listedPrice: number;
+  discountPrice: number;
+  is_hot: boolean;
+  listphotos: [];
+  createdAt: string;
+  updateAt: string;
+  deletedAt: string;
+  quantity: number;
+  img: string;
+  tags: [];
+  description: string;
+  sold: number;
+  vote: number;
+  supplier: string;
 }
 
-const initValue: IItemCart = {
-  cart: {
-    _id: '',
-    item: [
-      {
-        product_id: {
-          _id: '61b05332df535cdbd41aec55',
-          name: 'Du Lịch Hang Sơn Đoòng',
-          listedPrice: 15000000,
-          discountPrice: 1000000,
-          is_hot: true,
-          listphotos: [],
-          quantity: 100,
-          sold: 0,
-          vote: 5,
-          tags: [],
-          createdAt: '',
-          updateAt: '',
-          deletedAt:'',
-          img: '',
+export enum EStatusAuth {
+  check = 1,
+  unauth = 2,
+  auth = 3,
+}
+export interface IItemCart {
+  items: [
+    {
+      product_id: IProduct;
+    },
+  ];
+  quantity: number;
+  totalPrice: number;
+  _id: string;
+}
+export interface IAuth {
+  productId: IProduct[];
+  message: string;
+  statusAuth: EStatusAuth;
+  quantity: number;
+  total: number;
+}
 
-          description: '',
-          supplier: '',
-        },
-      },
-    ],
-  },
+export interface IResProduct {
+  success: boolean;
+  message: string;
+  product: IProduct[];
+}
+
+const initValue: IAuth = {
+  productId: [
+    {
+      _id: '',
+      name: '',
+      listedPrice: 0,
+      discountPrice: 0,
+      is_hot: false,
+      listphotos: [],
+      createdAt: '',
+      updateAt: '',
+      deletedAt: '',
+      quantity: 0,
+      img: '',
+      tags: [],
+      description: '',
+      sold: 0,
+      vote: 0,
+      supplier: '',
+    },
+  ],
+  message: '',
+  statusAuth: EStatusAuth.check,
+  quantity: 0,
+  total: 0,
 };
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: initValue,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const existingIndex = state.productId.findIndex(
+        item => item._id === action.payload._id,
+      );
+      if (existingIndex >= 0) {
+        state.productId[existingIndex] = {
+          ...state.productId[existingIndex],
+          quantity: state.productId[existingIndex].quantity + 1,
+        };
+      } else {
+        let tempProductItem = {...action.payload, quantity: 1};
+        state.productId.push(tempProductItem);
+      }
+    },
+    decreaseCart: (state, action) => {
+      const itemIndex = state.productId.findIndex(
+        item => item._id === action.payload.id,
+      );
+      if (state.productId[itemIndex].quantity > 1) {
+        state.productId[itemIndex].quantity -= 1;
+      } else if (state.productId[itemIndex].quantity === 1) {
+        const nextCartItems = state.productId.filter(
+          item => item._id !== action.payload.id,
+        );
 
-
-export const cartSlice = createSlice({
-    name:'cart',
-    initialState: initValue,
-    reducers:{
-        onGetCart:(state,action:PayloadAction<IItemCart>) =>{
-            state.cart = action.payload.cart;
+        state.productId = nextCartItems;
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.productId.map(cartItem => {
+        if (cartItem._id === action.payload.id) {
+          const nextCartItems = state.productId.filter(item => {
+            item._id !== cartItem._id;
+          });
+          state.productId = nextCartItems;
         }
-    }
-})
-export const {onGetCart} = cartSlice.actions;
-export default cartSlice.reducer
+        return state;
+      });
+    },
+    updateStatusAuth: (
+      state,
+      action: PayloadAction<{statusAuth: EStatusAuth}>,
+    ) => {
+      state.statusAuth = action.payload.statusAuth;
+    },
+
+    getTotals(state, action) {
+      let {total, quantity} = state.productId.reduce(
+        (cartTotal, cartItem) => {
+          const {discountPrice, quantity} = cartItem;
+          const itemTotal = discountPrice * quantity;
+
+          cartTotal.total += itemTotal;
+          cartTotal.quantity += quantity;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          quantity: 0,
+        },
+      );
+      total = parseFloat(total.toFixed(2));
+      state.quantity = quantity;
+      state.total = total;
+    },
+    clearCart(state, action) {
+      state.productId = [];
+    },
+  },
+});
+export const {
+  addToCart,
+  updateStatusAuth,
+  clearCart,
+  getTotals,
+  removeFromCart,
+  decreaseCart,
+} = CartSlice.actions;
+export default CartSlice.reducer;
