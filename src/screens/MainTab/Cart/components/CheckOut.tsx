caa1fd626747a55ac25fef51b1b7a2a002c130fb
Thân Văn Hiếu <vanhieuu99@gmail.com>
2021-12-25 18:37:00 +0700

    done checkout screen

--- src/screens/MainTab/Cart/components/CheckOut.tsx
+++ src/screens/MainTab/Cart/components/CheckOut.tsx
@@ -1,8 +1,16 @@
 import {useTheme} from '@shopify/restyle';
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
+import {Button} from 'react-native-elements';
+import {Text, View} from 'react-native-ui-lib';
+import {Colors} from 'react-native-ui-lib/typings';
+import {useSelector} from 'react-redux';
 import {Theme} from 'react-toastify';
 import Box from '../../../../components/Box';
+import {numberFormat} from '../../../../config/formatCurrency';
+import {IProduct} from '../../../../redux/authCartSlice';
+import {RootState} from '../../../../redux/store';
+import AddCard from './AddCard';
 import Card, {CardType} from './Card';
 
 interface CheckOutProps {
@@ -25,8 +33,52 @@ const cards = [
   },
 ];
 
+interface addressProps {
+  number: string;
+  city: string;
+  street: string;
+}
+
+const initStateAddress: addressProps = {
+  number: '23 ngách 17/2',
+  city: 'Hà Nội',
+  street: 'Nguyễn Văn Lộc',
+};
+
+interface ICart {
+  quantity: number;
+  label: string;
+  totalPrice: number;
+}
+const LineItem = ({totalPrice, label}: ICart) => {
+  return (
+    <Box flexDirection="row" paddingVertical="m">
+      <Box>
+        <Text color="#fff" h20>
+          ${label}
+        </Text>
+      </Box>
+      <Box>
+        <Text color={Colors.primary}>${numberFormat.format(totalPrice)}</Text>
+      </Box>
+    </Box>
+  );
+};
+
 const CheckOut = ({minHeight}: CheckOutProps) => {
+
+
+
+
+
+
+
   const [selectedCard, setSelectedCard] = React.useState(cards[0].id);
+  const [address, setAddress] = React.useState<addressProps>(initStateAddress);
+  const quantity = useSelector<RootState, number>(state => state.cart.quantity);
+  const totalPrice = useSelector<RootState, number>(
+    state => state.cart.totalPrice,
+  );
   const theme = useTheme<Theme>();
   return (
     <Box
@@ -35,18 +87,67 @@ const CheckOut = ({minHeight}: CheckOutProps) => {
       padding="m"
       style={{paddingTop: minHeight}}>
       <Box flex={1} padding="m">
-        <ScrollView horizontal>
-          {cards.map(card => {
-            return (
-              <Card
-                key={card.id}
-                card={card}
-                selected={selectedCard === card.id}
-                onSelected={() => setSelectedCard(card.id)}
-              />
-            );
-          })}
-        </ScrollView>
+        <Box height={160}>
+          <ScrollView horizontal>
+            <AddCard />
+            {cards.map(card => {
+              return (
+                <Card
+                  key={card.id}
+                  card={card}
+                  selected={selectedCard === card.id}
+                  onSelected={() => setSelectedCard(card.id)}
+                />
+              );
+            })}
+          </ScrollView>
+        </Box>
+        <Box marginTop="l">
+          <Text color="#fff" h16 bold marginB-10>
+            Địa chỉ giao hàng
+          </Text>
+          <Box flexDirection="row" opacity={0.5}>
+            <Box flex={1} paddingVertical="m" opacity={0.5}>
+              <Text color="#fff" h13>
+                {address.number}
+              </Text>
+              <Text color="#fff" h13>
+                {address.street}
+              </Text>
+              <Text color="#fff" h13>
+                {address.city}
+              </Text>
+            </Box>
+            <Box
+              justifyContent="center"
+              alignItems="center"
+              paddingVertical="m">
+              <TouchableOpacity>
+                <Text color="#fff">Change</Text>
+              </TouchableOpacity>
+            </Box>
+          </Box>
+          <LineItem
+            quantity={quantity}
+            label="Tổng tiền"
+            totalPrice={totalPrice}
+          />
+          <LineItem quantity={quantity} label="Phí dịch vụ " totalPrice={0} />
+          <LineItem
+            quantity={quantity}
+            label="Tổng tiền"
+            totalPrice={totalPrice}
+          />
+        </Box>
+        <Box
+          paddingVertical="l"
+          alignItems="center"
+          flex={1}
+          justifyContent="flex-end">
+          <View style={{backgroundColor: '#E9707D'}}>
+            <Button title={`Nhấn để thanh toán ${totalPrice} `} type="solid"  onPress={()=> true}/>
+          </View>
+        </Box>
       </Box>
     </Box>
   );
